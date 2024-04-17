8f3ef8657ef61a72e04b6b7643707a1c2d84cc94
MA-Mahmudi <ma.mahmudi18@outlook.com>
2024-04-17 22:18:08 +0330

    Convert to function

--- app.php
+++ app.php
@@ -1,41 +1,45 @@
 <?php
 
-$shares = ["mmd" => 0, "hossein" => 320, "k1" => 0, "Saleh" => 0, "Amin" => 410];
-$total = array_sum($shares);
-$negatives = [];
-$positives = [];
-$res = [];
+function calculateDong (array $shares): array {
+  $total = array_sum($shares);
+  $negatives = [];
+  $positives = [];
+  $res = [];
 
-$sharePerPerson = $total / count($shares);
+  $sharePerPerson = $total / count($shares);
 
-foreach ($shares as $key => $value) {
-  $difference = $value - $sharePerPerson;
-  if ($difference < 0) {
-    $negatives[$key] = $difference;
-  } else {
-    $positives[$key] = $difference;
-  }
-}
-
-ksort($negatives);
-ksort($positives);
-
-foreach ($negatives as $nKey => $temp1) {
-  foreach ($positives as $pKey => $temp2) {
-    if ($negatives[$nKey] !== 0 && $positives[$pKey] !== 0) {
-
-      $transferAmount = min(abs($negatives[$nKey]), $positives[$pKey]);
-
-      $resKey = $nKey . " to " . $pKey;
-
-      $res[$resKey] = $transferAmount;
-
-      $negatives[$nKey] += $transferAmount;
-
-      $positives[$pKey] -= $transferAmount;
-
+  foreach ($shares as $key => $value) {
+    $difference = $value - $sharePerPerson;
+    if ($difference < 0) {
+      $negatives[$key] = $difference;
+    } else {
+      $positives[$key] = $difference;
     }
   }
-}
 
-print_r($res);
+  ksort($negatives);
+  ksort($positives);
+
+  foreach ($negatives as $nKey => $temp1) {
+    foreach ($positives as $pKey => $temp2) {
+      if ($negatives[$nKey] !== 0 && $positives[$pKey] !== 0) {
+
+        $transferAmount = min(abs($negatives[$nKey]), $positives[$pKey]);
+
+        $resKey = $nKey . " to " . $pKey;
+
+        $res[$resKey] = $transferAmount;
+
+        $negatives[$nKey] += $transferAmount;
+
+        $positives[$pKey] -= $transferAmount;
+
+      }
+    }
+  }
+
+  return $res;
+}
+$shares = ["a" => 0, "b" => 320, "c" => 0, "d" => 0, "e" => 410];
+
+print_r(calculateDong($shares));
