12bb169cf66eed9f7d6f524bb763aa04e7eeba7b
MA-Mahmudi <ma.mahmudi18@outlook.com>
2024-04-17 21:38:59 +0330

    init

---
+++ app.php
@@ -0,0 +1,41 @@
+<?php
+
+$shares = ["mmd" => 0, "hossein" => 320, "k1" => 0, "Saleh" => 0, "Amin" => 410];
+$total = array_sum($shares);
+$negatives = [];
+$positives = [];
+$res = [];
+
+$sharePerPerson = $total / count($shares);
+
+foreach ($shares as $key => $value) {
+  $difference = $value - $sharePerPerson;
+  if ($difference < 0) {
+    $negatives[$key] = $difference;
+  } else {
+    $positives[$key] = $difference;
+  }
+}
+
+ksort($negatives);
+ksort($positives);
+
+foreach ($negatives as $nKey => $temp1) {
+  foreach ($positives as $pKey => $temp2) {
+    if ($negatives[$nKey] !== 0 && $positives[$pKey] !== 0) {
+
+      $transferAmount = min(abs($negatives[$nKey]), $positives[$pKey]);
+
+      $resKey = $nKey . " to " . $pKey;
+
+      $res[$resKey] = $transferAmount;
+
+      $negatives[$nKey] += $transferAmount;
+
+      $positives[$pKey] -= $transferAmount;
+
+    }
+  }
+}
+
+print_r($res);
