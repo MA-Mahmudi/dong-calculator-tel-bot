045507ff12e3258331a8388a5a68931ea3eff409
ma-mahmudi <ma.mahmudi18@outlook.com>
2024-04-20 13:39:30 +0330

    Checking if the input is correct

--- src/config-bot.php
+++ src/config-bot.php
@@ -10,31 +10,37 @@ $env = parse_ini_file('.env');
 $botApiKey = $env["API_TOKEN"];
 
 try {
-    $bot = new Nutgram($botApiKey);
+    $bot = new Nutgram($botApiKey, ["timeout" => 60000]);
 
     $bot->onCommand('start', function (Nutgram $bot) {
-        $message = "فرمت پیام به صورت:" . PHP_EOL . "نام: مبلغ" . PHP_EOL . "نام: مبلغ" . PHP_EOL . "نام: مبلغ";
+        $message = "فرمت پیام به صورت:" . PHP_EOL . "نام:مبلغ" . PHP_EOL . "نام:مبلغ" . PHP_EOL . "نام:مبلغ";
         $bot->sendMessage($message);
     });
 
     $bot->onMessage(function (Nutgram $bot) {
         $message = $bot->message()->getText();
-        $temp = explode("\n", $message);
-        $res = [];
-        $resMessage = "";
-        foreach ($temp as $person) {
-            $temp2 = explode(":", $person);
-            $res[trim($temp2[0])] = trim($temp2[1]);
-        }
-        print_r($res);
-        $dongs = calculateDong($res);
-        print_r($dongs);
-        foreach ($dongs as $key => $value) {
-            if ($value != 0) {
-                $resMessage .= $key . ": " . $value . "\n";
+        $isValidFlag = true;
+        foreach (explode("\n",$message) as $item){
+            if(!preg_match("/^[@\x{0600}-\x{06FF}a-zA-Z\d]+:\d*$/u", $item)){
+                $isValidFlag = false;
             }
         }
-        $bot->sendMessage($resMessage);
+        if ($isValidFlag){
+            $temp = explode("\n", $message);
+            $res = [];
+            $resMessage = "";
+            foreach ($temp as $person) {
+                $temp2 = explode(":", $person);
+                $res[trim($temp2[0])] = trim($temp2[1]);
+            }
+            $dongs = calculateDong($res);
+            foreach ($dongs as $key => $value) {
+                if ($value != 0) {
+                    $resMessage .= $key . ": " . $value . "\n";
+                }
+            }
+            $bot->sendMessage($resMessage);
+        }
     });
 
     $bot->run();
